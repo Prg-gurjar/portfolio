07bbb9e1ed7d558f3880898169048ecf24908346
Prg-gurjar <157681075+Prg-gurjar@users.noreply.github.com>
2024-09-27 21:30:41 +0530

    portfolio

---
+++ PORTFOLIO/src/components/About.jsx
@@ -0,0 +1,76 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div name="About" className='max-w-screen-2xl container mx-auto px-4 md:px-20 my-16 '>
+        <div>
+        <h1 className='text-3xl font-bold mb-5'>About</h1>
+        <p>
+          Hello, I'm Pragati, a passionate Full Stack developer with a keen eye for Full stack Developer . With a background in IT, I strive to create impactful and
+          visually stunning Software solutions that leave a lasting impression
+        </p>
+        <br/>
+        <h1 className='text-green-600 font-semibold text-l'>
+            Education & Learning
+        </h1>
+        <span>
+          Currently pursuing My Master in Computer Application from National Institute Of Technology Raipur and
+          I did my bachelors in Computer Science from Devi Ahilya University Indore which was completed in 2022.
+          I did my schooling in my hometown under Mp Board which is situated in Khargone district of Madhya Pradesh .
+          
+        </span>
+        <br/>
+        <br/>
+        <h1 className='text-green-600 font-semibold text-l'>
+            Skills & Expertise
+        </h1>
+        <span> 
+          Proficient in CPP, Java, Python and Basic of C Experienced with Visual Studio Code, Eclipse ,Jupitor Notebook Strong grasp of 
+          Excellent problem-solving skills Effective communicator and
+          collaborator
+        </span>
+        <br/>
+        <br/>
+        <h1 className='text-green-600 font-semibold text-l'>
+            Professional Experience
+        </h1>
+        <span>
+        Project Developer, Personal Project Developed a web application using HTML, CSS, and 
+        JavaScript. Implemented features such as user authentication and data storage . 
+        Collaborated with myself  to deliver the project within a month.</span><br/>
+        <span>Relevant Coursework :=</span><br/>
+        <span>•Data Structures and Algorithms </span><br/>
+        <span>•Computer Network </span><br/>
+        <span>•Database Management System,SQL</span><br/>
+        <span>•Operating System</span><br/>
+        <span>• OOPS</span><br/>
+        <span>Programming languages := CPP,Java, Python, Basic of C. </span><br/>
+        <span>Technologies := HTML, CSS, React-Js,Spring,SpringBoot.</span><br/>
+        <span>Tools := Git, Visual Studio Code,Eclipse</span><br/>
+        
+        <br/>
+        <br/>
+        <h1 className='text-green-600 font-semibold text-l'>
+          Achievements & Awards
+        </h1>
+        <span>•	Hacker Rank Problem Solving (Basic of C) Certificate </span><br/>
+        <span>•	Coding Ninja Java (Basic) Certificate</span><br/>
+        <span>•	Coding Ninja Html, CSS , JavaScript Certificate</span>
+        <br/>
+        <br/>
+        <h1 className="text-green-600 font-semibold text-l">
+          Mission Statement
+        </h1>
+        <p>
+          My mission is to leverage my skills and creativity to deliver
+          innovative Software Developer, solutions that exceed client expectations and
+          contribute positively to the digital landscape. I am committed to
+          continuous learning and growth, always seeking new challenges and
+          opportunities to expand my horizons.
+        </p>
+        </div>
+    </div>
+  )
+}
+
+export default About  
